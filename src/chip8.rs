e0f7d2538c5488d5dc89f4591a4875f11d4618da
Huy tran <huy.td183768@sis.hust.edu.vn>
2021-10-12 00:24:00 +0700

    feat: handle basic instructions

--- src/chip8.rs
+++ src/chip8.rs
@@ -9,6 +9,7 @@ pub struct Chip8Interpreter {
 }
 
 enum Instruction {
+    End(u16),
     Clear(u16),
     Jump(u16),
     Sevx(u16),
@@ -20,14 +21,36 @@ type Mem = [u8; 4096];
 
 impl Chip8Interpreter {
     pub fn new() -> Chip8Interpreter {
+        let mut mem = [0; 4096];
+        let fonts = [
+            0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+            0x20, 0x60, 0x20, 0x20, 0x70, // 1
+            0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+            0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+            0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+            0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+            0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+            0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+            0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+            0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+            0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+            0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+            0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+            0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+            0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+            0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+        ];
+        for x in 0..fonts.len() {
+            mem[x] = fonts[x];
+        }
         Chip8Interpreter {
             registers_v: [0; 16],
             registers_i: [0; 2],
             delay_timer: 0,
             sound_timer: 0,
-            register_pc: 0,
-            mem: [0; 4096],
+            register_pc: 0x200,
             pixels: [[0; 64]; 32],
+            mem,
         }
     }
 
@@ -37,7 +60,7 @@ impl Chip8Interpreter {
             panic!("File too long!!");
         }
         for (idx, &byte) in file.iter().enumerate() {
-            self.mem[idx] = byte;
+            self.mem[0x200+idx] = byte;
         }
     }
 
@@ -49,7 +72,9 @@ impl Chip8Interpreter {
     }
 
     fn exec(&mut self) {
-        panic!("Not implemented!");
+        let opcode = self.fetch(self.register_pc);
+        let instruction = self.decode(opcode);
+        self.execute(instruction);
     }
 
     fn fetch(&mut self, addr: u16) -> u16{
@@ -68,7 +93,11 @@ impl Chip8Interpreter {
         self.mem[self.registers_i[0] as usize]
     }
     
-    fn decode(opcode: u16) -> Instruction{
+    fn decode(&self, opcode: u16) -> Instruction{
+        if opcode == 0 {
+            // std::process::exit(0);
+            return Instruction::End(opcode);
+        }
         if opcode == 0x00E0 {
             return Instruction::Clear(opcode);
         }
@@ -78,17 +107,23 @@ impl Chip8Interpreter {
         if opcode >> 12 == 0x6 {
             return Instruction::Sevx(opcode);
         }
+        if opcode >> 12 == 0x7 {
+            return Instruction::Advx(opcode);
+        }
         if opcode >> 12 == 0xA {
             return Instruction::Seri(opcode);
         }
         if opcode >> 12 == 0xD {
-            return Instruction::Seri(opcode);
+            return Instruction::Draw(opcode);
         }
-        panic!("Unknow opcode!")
+        panic!("Unknow opcode {:#04x} at pc {}!", opcode, self.register_pc);
     }
     
     fn execute(&mut self, inst: Instruction) {
         match inst {
+            Instruction::End(_) => {
+                std::process::exit(0);
+            }
             Instruction::Clear(opcode) => {
                 self.pixels = [[0; 64]; 32];
             }
@@ -110,13 +145,17 @@ impl Chip8Interpreter {
                 self.registers_i[0] = nnn;
             }
             Instruction::Draw(opcode) => {
-                let sprite_tall = self.take_mem_at_vi();
                 let x = take_param_x(opcode);
                 let y = take_param_y(opcode);
                 let n = take_param_n(opcode);
                 let x_cor = self.registers_v[x as usize] & 63;
                 let y_cor = self.registers_v[y as usize] & 31;
+                println!("x,y {}, {}", x_cor, y_cor);
+                println!("mem {:#04x}", self.registers_i[0] as usize);
+                println!("at {}", self.mem[self.registers_i[0] as usize]);
                 self.registers_v[0xF] = 0;
+                self.registers_v[0xF] = display(&mut self.pixels, self.mem, self.registers_i[0], x_cor, y_cor, n as u16);
+                self.display();
             }
         }
     }
@@ -142,20 +181,20 @@ fn take_param_y(opcode: u16) -> u8 {
     ((opcode & 0x00F0) >> 4) as u8
 }
 
-fn display(pixels: &mut [[u8; 64]; 32], mem: Mem, i: u16, x_cor: u16, y_cor: u16, n: u16) -> bool {
-    let mut ret = false;
+fn display(pixels: &mut [[u8; 64]; 32], mem: Mem, i: u16, x_cor: u16, y_cor: u16, n: u16) -> u16 {
+    let mut ret = 0;
     for row in 0..n {
         let mut sprite = mem[(i + row) as usize];
-        for x in 1..8{
-            if sprite & 0x80 > 0 {
+        for x in 0..8{
+            if sprite >> 7 > 0 {
                 let to_y = (y_cor + row) as usize;
                 let to_x = (x_cor + x) as usize;
-                if to_x < 32 && to_y < 64 {
-                    pixels[(y_cor + row) as usize][(x_cor + x) as usize] = 1;
-                    ret = true;
+                if to_x < 64 && to_y < 32 {
+                    pixels[to_y][to_x] ^= 1;
+                    ret = 1;
                 }
             }
-            sprite >>= 1;
+            sprite <<= 1;
         }
     }
 
@@ -216,10 +255,19 @@ mod tests {
     // #[ignore]
     fn test_cpu_display() {
         let mut cpu = Chip8Interpreter::new();
-        // let sprite = [[1, 1, 1, 1, 0, 0, 0, 0]];
-        cpu.mem[0] = 0b1111;
-        cpu.pixels = [[0; 64]; 32];
-        display(&mut cpu.pixels, cpu.mem, 0, 0, 0, 3);
+        cpu.mem[0] = 0b11111111;
+        cpu.mem[1] = 0b01111111;
+        cpu.pixels = [[1; 64]; 32];
+        assert_eq!(display(&mut cpu.pixels, cpu.mem, 0, 63, 31, 1), 1);
+        cpu.display();
+        assert_eq!(display(&mut cpu.pixels, cpu.mem, 1, 63, 31, 1), 0);
+        cpu.display();
+    }
+
+    #[test]
+    fn test_ibm_logo() {
+        let mut cpu = Chip8Interpreter::new();
+        cpu.run_rom("ibmrom.ch8");
         cpu.display();
     }
 }
